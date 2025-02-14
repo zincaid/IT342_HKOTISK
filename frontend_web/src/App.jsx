c6a6483145699875fc153a412b5a6cdaa5004218
LAB23_USER ON WS19 <L23X14W19@citulab.net>
2025-02-14 13:37:49 +0800

    frontend_web hello world

---
+++ frontend_web/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className="hello-world">
+          Hello World!!
+      </p>
+    </>
+  )
+}
+
+export default App
