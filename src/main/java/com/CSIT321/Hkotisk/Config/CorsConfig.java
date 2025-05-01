0e2ef8782047d95c8ead83c73e8f2c56bfcff9b8
ohpie69 <bencadayona@gmail.com>
2025-05-02 04:32:07 +0800

    changed cors policy

--- src/main/java/com/CSIT321/Hkotisk/Config/CorsConfig.java
+++ src/main/java/com/CSIT321/Hkotisk/Config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:5173", "https://accounts.google.com", "https://login.microsoftonline.com")
+                    .allowedOrigins("http://localhost:5173", "https://accounts.google.com", "https://login.microsoftonline.com", "https://it-342-hkotisk-4q3i.vercel.app")
                     .allowedMethods("*")
                     .allowedHeaders("*")
                     .allowCredentials(true);
